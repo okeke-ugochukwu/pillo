f4132602c63bf2d63195523e1142bab8cf42798b
mr.pianoman_ <success.US7@gmail.com>
2020-07-29 16:22:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openSideBar() {
+    document.getElementById("sideBar").style.marginLeft = "0";
+}
+function closeSideBar() {
+    document.getElementById("sideBar").style.marginLeft = "-350px";
+}
